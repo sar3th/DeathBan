beaadcd5bd17904d73ec932b51c7d18d838b960e
sar3th <sar3th@gmail.com>
2012-03-13 01:53:36 +0100

    Added Main class

---
+++ src/com/github/sar3th/DeathBan/DeathBan.java
@@ -0,0 +1,14 @@
+package com.github.sar3th.DeathBan;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Main class of DeathBan plugin for Bukkit
+ */
+public class DeathBan extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // TODO: Code goes here
+    }
+}
