e6cd65c3cd746b99dfc3ea7ecf2d4bbb0c747b8b
sar3th <sar3th@gmail.com>
2012-03-13 02:50:35 +0100

    Added db-settime command

--- src/com/github/sar3th/DeathBan/DeathBan.java
+++ src/com/github/sar3th/DeathBan/DeathBan.java
@@ -1,14 +1,51 @@
 package com.github.sar3th.DeathBan;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * Main class of DeathBan plugin for Bukkit
  */
-public class DeathBan extends JavaPlugin {
+public class DeathBan extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // TODO: Code goes here
+        // Register as event Listener
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+    
+    @EventHandler
+    public void onPlayerLogin(PlayerLoginEvent event) {
+        if (isEnabled()) {
+            // TODO: Add code to kick player if he's banned
+        }
+    }
+    
+    @EventHandler
+    public void onEntityDeathEvent(EntityDeathEvent event) {
+        if (isEnabled()) {
+            // TODO: Add code to detect player death and add a ban
+        }
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("db-settime")) {
+            if (args.length == 1) {
+                int banTime = Integer.parseInt(args[0]);
+                getConfig().set("bantime", banTime);
+                saveConfig();
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
 }
