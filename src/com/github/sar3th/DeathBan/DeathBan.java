9fda9068caf9b62d119fc98a3c27e0a2a0ff0efa
sar3th <sar3th@gmail.com>
2012-03-26 00:19:56 +0200

    Fixed unban command again

--- src/com/github/sar3th/DeathBan/DeathBan.java
+++ src/com/github/sar3th/DeathBan/DeathBan.java
@@ -175,9 +175,14 @@ public class DeathBan extends JavaPlugin implements Listener {
             }
         } else if (command.getName().equalsIgnoreCase("db-unban")) {
             if (args.length == 1) {
-                String targetPlayer = getServer().getPlayer(args[0]).getName();
-                if (targetPlayer == null)
+                Player player = getServer().getPlayer(args[0]);
+                String targetPlayer;
+                
+                if (player == null) {
                     targetPlayer = args[0];
+                } else {
+                    targetPlayer = player.getName();
+                }
                 
                 Long bantime = banDatabase.get(targetPlayer.toLowerCase());
                 if (bantime != null) {
